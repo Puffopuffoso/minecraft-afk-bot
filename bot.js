8c93df9f97d783595cb0f92ac4218c0ee8205941
Puffopuffoso <95428972+Puffopuffoso@users.noreply.github.com>
2026-03-16 15:43:54 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,72 +1,50 @@
 const mineflayer = require('mineflayer')
 
-let bot = null
-
-function startBot() {
-
-  if (bot) {
-    console.log("Bot già acceso")
-    return
-  }
-
-  bot = mineflayer.createBot({
-    host: "ttuffsmp.falixsrv.me",
-    port: 27615,
-    username: "wallstdokicte8@hotmail.com",
-    auth: "microsoft",
-    version: "1.21.1"
-  })
-
-  bot.once("spawn", () => {
-    console.log("BOT ONLINE SU TUFFSMP")
-
-    setTimeout(() => {
-      bot.chat("/login 12345678")
-    }, 5000)
-  })
-
-  bot.on("chat", (username, message) => {
-
-    if (username === bot.username) return
-
-    // spegne il bot
-    if (message === "!bot off") {
-      bot.chat("Bot offline")
-      bot.quit()
-      bot = null
-    }
-
-  })
-
-  bot.on("kicked", (reason) => {
-    console.log("KICKATO:", reason)
-  })
-
-  bot.on("error", (err) => {
-    console.log("ERRORE:", err)
-  })
-
-  bot.on("end", () => {
-    console.log("Bot uscito dal server")
-    bot = null
-  })
-
-}
-
-// comando da minecraft per accendere
-const mineflayerListener = mineflayer.createBot({
+const bot = mineflayer.createBot({
   host: "ttuffsmp.falixsrv.me",
   port: 27615,
-  username: "controller_bot",
+  username: "wallstdokicte8@hotmail.com",
+  auth: "microsoft",
   version: "1.21.1"
 })
 
-mineflayerListener.on("chat", (username, message) => {
+let afk = false
 
-  if (username === mineflayerListener.username) return
+bot.once("spawn", () => {
+  console.log("BOT ONLINE")
 
-  if (message === "!bot on") {
-    startBot()
+  setTimeout(() => {
+    bot.chat("/login 12345678")
+  }, 5000)
+})
+
+bot.on("chat", (username, message) => {
+
+  if (username === bot.username) return
+
+  if (message === "!afk on") {
+    afk = true
+    bot.chat("AFK attivato")
+  }
+
+  if (message === "!afk off") {
+    afk = false
+    bot.chat("AFK disattivato")
   }
 
 })
+
+setInterval(() => {
+
+  if (!afk) return
+
+  bot.setControlState("jump", true)
+
+  setTimeout(() => {
+    bot.setControlState("jump", false)
+  }, 500)
+
+}, 30000)
+
+bot.on("kicked", console.log)
+bot.on("error", console.log)
